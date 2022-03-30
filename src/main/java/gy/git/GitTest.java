38aeadb82c0edebc25dc1813781fd1dd4e22a5c2
GYgoooo <102585039+GYgoooo@users.noreply.github.com>
2022-03-30 11:35:26 +0800

    Update GitTest.java

--- src/main/java/gy/git/GitTest.java
+++ src/main/java/gy/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello git   master");
         System.out.println("hello git   hot fix222");
         System.out.println("hello git   push test");
+        System.out.println("hello git   pull test");
     }
 }
