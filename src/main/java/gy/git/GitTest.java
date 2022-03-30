e531342a877fe3f57f6747fb05faf320cc07e320
gy <gy@qq.com>
2022-03-30 11:01:52 +0800

    third

--- src/main/java/gy/git/GitTest.java
+++ src/main/java/gy/git/GitTest.java
@@ -2,6 +2,6 @@ package gy.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git222");
+        System.out.println("hello git333");
     }
 }
